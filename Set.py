6339a532d1d0dfc0c62526a99a3faa95e181dad5
Shubham Krishna shinde <118288116+shubhamSS9@users.noreply.github.com>
2024-05-15 06:47:58 +0530

    Add files via upload

---
+++ Set.py
@@ -0,0 +1,92 @@
+# c = {76, "shubham", 87.089}
+# print(c)
+
+# a = set()
+# print(a)
+
+# b = {20}
+# print(b)
+
+# d = {10, 10, 10, 10, 10}
+# print(d)
+
+# s = {10, 13, 14, 17, 18, 19, 3999}
+# print(s)
+# for i in s: 
+#     print(i)
+
+# s = {"gate", "fate", "late"}
+# # print(s.add("shubham"))
+# s.add("shubham")
+# print(s)
+
+# print(max(s))
+# print(len(s))
+# print(min(s))
+# print(sorted(s))
+# print(any(s))
+# print(all(s))
+
+
+# num = {25, -8237, 87487, 8373, 9.9287, -92892.20209}
+# # print(sum(num))
+# print(s is num)
+
+# a = [10]
+# b = [10.00]
+# print(a == b)
+# print(a is b)
+
+# num = {25, 8237, 87487, 8373, 9.9287, 92892.20209}
+# t = {"A", "B", "C"}
+
+# # u = set()
+
+# num.add("Shubham")
+# # print(num)
+
+# num.update(t)
+# print(num)
+
+# u = num.copy()
+# print(u)
+
+# num.remove(8237)
+# print(num)
+
+# num.discard(25)
+# print(num)
+
+# num.discard(2563647)
+# print(num)
+
+# num.discard("jgufvbb")
+# print(num)
+
+# num.clear()
+# print(num)
+
+# enginees = {"shubham", "sanjay", "ajay", "sujay", "dinesh"}
+# managers = {"mohan", "sanjay"}
+
+# print(enginees | managers)
+# print(enginees & managers)
+# print(enginees - managers)
+# print(managers - enginees)
+# print(enginees ^ managers)
+# print(managers ^ enginees)
+
+# a = {1, 4, 5}
+# b = {2, 4, 5}
+# print(a <= b)
+# print(a | b)
+# print(a & b)
+# print(a - b)
+# # print(a + b)
+# print(a ^ b)
+
+# c = {6, 4, 23, *a, 76, *b, 89}
+# print(c)
+
+# x = {1, 2, 3, 4}
+# print(*x)
